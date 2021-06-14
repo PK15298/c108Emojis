19bc106d22d984d5b72ef2439c8c6b2a6fdf68e2
poonam2096740 <85899531+poonam2096740@users.noreply.github.com>
2021-06-15 03:03:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+prediction1 = ""
+prediction2 = ""
+
+
+function takesnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id='capturedimage' src='" + data_uri + "'/>";
+    });
+}
+console.log("ml5 version: ", ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json',modelLoaded);
+
+
+
+function modelLoaded(){
+console.log("Model Loaded");
+}
+
+
+
+function speak(){
+var synth =window.speechSynthesis;
+speakdata1 = "the first prediction is "+prediction1;
+speakdata2 = "and the second prediction is"+prediction2;
+var utterThis=new SpeechSynthesisUtterance(speakdata1+speakdata2);
+synth.speak(utterThis);
+}
+
+
+
+function check(){
+img = document.getElementById("captured_image");
+classifier.classify(img,gotResult);
+}
+
+
+function gotResult(error,results){
+if(error){
+console.error(error);
+}else{
+console.log(results);
+document.getElementById("result_name").innerHTML=results[0].label;
+document.getElementById("result_name2").innerHTML=results[1].label;
+prediction1=results[0].label;
+prediction2=results[1].label;
+speak();
+if(prediction1=="happy"){
+document.getElementById("update_emoji").innerHTML="&#128522;";
+}
+if(prediction1=="sad"){
+    document.getElementById("update_emoji").innerHTML="&#128532;";
+    }
+    if(prediction1=="angry"){
+        document.getElementById("update_emoji").innerHTML="&#128548;";
+        }
+        if(prediction2=="happy"){
+            document.getElementById("update_emoji").innerHTML="&#128522;";
+            }
+            if(prediction2=="sad"){
+                document.getElementById("update_emoji").innerHTML="&#128532;";
+                }
+                if(prediction2=="angry"){
+                    document.getElementById("update_emoji").innerHTML="&#128548;";
+                    }
+}
+}

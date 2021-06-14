6463b72d637bc32a62a93c64d954f0a6a335113a
poonam2096740 <85899531+poonam2096740@users.noreply.github.com>
2021-06-15 03:54:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ prediction2 = ""
 
 function takesnapshot() {
     Webcam.snap(function (data_uri) {
-        document.getElementById("result").innerHTML = "<img id='capturedimage' src='" + data_uri + "'/>";
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
-console.log("ml5 version: ", ml5.version);
+console.log('ml5 version:', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json',modelLoaded);
 
 
